88d7fec1f5254d99db971e783c87f968d7d8914f
Nathan Edmund Fanlau <nathanfanlau@gmail.com>
2024-08-30 21:40:01 +0700

    Make UI of calculator

---
+++ scripts.js
@@ -0,0 +1,30 @@
+main();
+
+function main() {
+    
+
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
+function changeSign(a){
+    return a * -1;
+}
