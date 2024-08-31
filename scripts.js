04bad7d0c574f97883932ac15ef01f2d684967d1
Nathan Edmund Fanlau <nathanfanlau@gmail.com>
2024-08-31 15:08:50 +0700

    Implement operate function

--- scripts.js
+++ scripts.js
@@ -1,86 +1,96 @@
 main();
 
 function main() {
-  let display = document.querySelector("#display");
-  let clearBtn = document.querySelector("#clear");
-  let changeSignBtn = document.querySelector("#changeSign");
-  let buttons = document.querySelectorAll(".number");
-  let operands = document.querySelectorAll(".operand");
-  let operateBtn = document.querySelector("#operate");
-  let firstNum, operand, secondNum;
+	let display = document.querySelector("#display");
+	let clearBtn = document.querySelector("#clear");
+	let changeSignBtn = document.querySelector("#changeSign");
+	let buttons = document.querySelectorAll(".number");
+	let operands = document.querySelectorAll(".operand");
+	let operateBtn = document.querySelector("#operate");
+	let firstNum, operand, secondNum;
 
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", () => {
-      if (operand && !secondNum) display.textContent = "";
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", () => {
+			if (operand && !secondNum) display.textContent = "";
 
-      let num = display.textContent.concat(buttons[i].value);
-      display.textContent = num;
+			let num = display.textContent.concat(buttons[i].value);
+			display.textContent = num;
 
-      if (!operand) firstNum = parseFloat(num);
-      else secondNum = parseFloat(num);
+			if (!operand) firstNum = parseFloat(num);
+			else secondNum = parseFloat(num);
 
-      // console.log(firstNum, secondNum, operand);
-    });
-  }
+			// console.log(firstNum, secondNum, operand);
+		});
+	}
 
-  for (let i = 0; i < operands.length; i++) {
-    operands[i].addEventListener("click", () => {
-      operand = operands[i].value;
+	for (let i = 0; i < operands.length; i++) {
+		operands[i].addEventListener("click", () => {
+			operand = operands[i].value;
 
-      // console.log(firstNum, secondNum, operand);
-    });
-  }
+			// console.log(firstNum, secondNum, operand);
+		});
+	}
 
-  clearBtn.addEventListener("click", () => {
-    display.textContent = "";
-    firstNum = secondNum = operand = null;
-  });
-  changeSignBtn.addEventListener("click", () => {
-    display.textContent = changeSign(display.textContent);
-  });
-  operateBtn.addEventListener("click", () => {
-    switch (operand) {
-      case "add":
+	clearBtn.addEventListener("click", () => {
+		display.textContent = "";
+		firstNum = secondNum = operand = null;
+	});
+	changeSignBtn.addEventListener("click", () => {
+		display.textContent = changeSign(display.textContent);
+	});
+	operateBtn.addEventListener("click", () => {
+		firstNum = operate(firstNum, secondNum, operand);
+		display.textContent = firstNum;
+		secondNum = null;
 
-        break;
-      case "subtract":
+		// console.log(firstNum, secondNum, operand);
+	});
+}
 
-        break;
-      case "multiply":
-
-        break;
-      case "divide":
-
-        break;
-      case "modulo":
-
-        break;
-      default:
-        break;
-    }
-  });
+function operate(firstNum, secondNum, operand){
+	let num;
+	switch (operand) {
+		case "add":
+			num = add(firstNum, secondNum);
+			break;
+		case "subtract":
+			num = subtract(firstNum, secondNum);
+			break;
+		case "multiply":
+			num = multiply(firstNum, secondNum);
+			break;
+		case "divide":
+			num = divide(firstNum, secondNum);
+			break;
+		case "modulo":
+			num = modulo(firstNum, secondNum);
+			break;
+		default:
+			break;
+	}
+	return num;
 }
 
 function add(a, b) {
-  return a + b;
+	return a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+	return a - b;
 }
 
 function multiply(a, b) {
-  return a * b;
+	return a * b;
 }
 
 function divide(a, b) {
-  return a / b;
+	return a / b;
 }
 
 function modulo(a, b) {
-  return a % b;
+	return a % b;
 }
 
 function changeSign(a) {
-  return parseFloat(a) * -1;
+	return parseFloat(a) * -1;
 }
